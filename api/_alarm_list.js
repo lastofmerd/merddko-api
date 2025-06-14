a3e2e7f7cd395531306216e0c76a44f90e3c6659
lastofmerd <avmertismetaytekin@gmail.com>
2025-06-14 13:14:18 +0300

    Create _alarm_list.js

---
+++ api/_alarm_list.js
@@ -0,0 +1,42 @@
+// _alarm_list.js
+
+function getItemList() {
+  return [
+    "Raptor",
+    "Shard",
+    "Hell Breaker",
+    "Iron Bow",
+    "MS+8",
+    "Lycaon Hammer",
+    "Lugias",
+    "Avedon",
+    "Iron Impact",
+    "Exceptional Blade Axe",
+    "Hanguk Sword",
+    "Totamic Spear",
+    "Dark Vane",
+    "Priest Impact",
+    "Holy Animor",
+    "Smite Hammer",
+    "Garp",
+    "Chaotic Staff",
+    "Scorching Staff",
+    "Elysium",
+    "Hell Blood",
+    "Glacier Erenion",
+    "Fire Erenion",
+    "Lightning Erenion",
+    "Flame Ring",
+    "Imir Ring",
+    "Shio Tears",
+    "Elemental Pendant",
+    "Iron Necklace",
+    "Iron Belt",
+    "Glass Belt",
+    "Warrior Pendant"
+  ];
+}
+
+module.exports = {
+  getItemList
+};
