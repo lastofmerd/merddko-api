4d65510a31deae877d01c0a7f55fd2d28fece60b
lastofmerd <avmertismetaytekin@gmail.com>
2025-06-14 11:17:00 +0300

    Add files via upload

---
+++ alarm_list.js
@@ -0,0 +1,7 @@
+const alarmList = [
+  { name: "Iron Bow", server: "Zero 3" },
+  { name: "Hell Breaker", server: "Zero 4" },
+  { name: "Rogue Chitin Pauldron", server: "Zero 5" }
+];
+
+export default alarmList;
