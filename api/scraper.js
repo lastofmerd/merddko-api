178d7605c300b208bf1ddf0cfc3c0e90bb139292
lastofmerd <avmertismetaytekin@gmail.com>
2025-06-14 18:39:37 +0300

    Update scraper.js

--- api/scraper.js
+++ api/scraper.js
@@ -1,10 +1,9 @@
-const { getItemList } = require('./_alarm_list');
+import { getItemList } from './_alarm_list.js';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const items = getItemList();
 
-    // 🔥 Alarm filtresi: fiyat, ortalamanın 1/5'inden düşükse
     const alarmItems = items.filter(item => item.price < item.average / 5);
 
     res.status(200).json({
@@ -15,4 +14,4 @@ module.exports = async (req, res) => {
     console.error('Scraper failed:', error);
     res.status(500).json({ error: 'Scraper failed', details: error.message });
   }
-};
+}
